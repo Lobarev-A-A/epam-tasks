a1f2e49ec21a9160668a722888c544b2685b4ddc
Lobarev A.A <andrewlobarev.acc@gmail.com>
2019-01-04 13:21:00 +0300

    Переделана функция ввода параметров, переделано представление параметров.

--- src/TaskThree.java
+++ src/TaskThree.java
@@ -5,33 +5,43 @@ import static java.lang.Math.*;
 public class TaskThree {
     public static void main(String[] args) {
         Scanner consoleInput = new Scanner(System.in);
-        double[] parameterVector = new double[3];         // [0]-a, [1]-b, [2]-h
-        do {
-            parameterVector = readParameters(consoleInput);
-        } while ((parameterVector[0] > parameterVector[1]) || (parameterVector[2] <= 0));
+        double a;
+        double b;
+        double h;
+        System.out.println("a <= b; h > 0");
+        a = readDouble("a = ", consoleInput);
+        b = readDouble("b = ", consoleInput);
+        h = readDouble("h = ", consoleInput);
 
-        printTable(parameterVector);
+        // Validation
+        while ((a > b) || (h <= 0)) {
+            System.out.println("Invalid value!");
+            if (a > b) {
+                System.out.println("a <= b");
+                a = readDouble("a = ", consoleInput);
+                b = readDouble("b = ", consoleInput);
+            }
+            if (h <= 0) {
+                System.out.println("h > 0");
+                h = readDouble("h = ", consoleInput);
+            }
+        }
+
+        printTable(a, b, h);
     }
 
-    private static double[] readParameters(Scanner consoleInput) {
-        System.out.println("a <= b; h > 0");
-        double[] bufParamVector = new double[3];
-        System.out.print("a = ");
-        bufParamVector[0] = consoleInput.nextDouble();
-        System.out.print("b = ");
-        bufParamVector[1] = consoleInput.nextDouble();
-        System.out.print("h = ");
-        bufParamVector[2] = consoleInput.nextDouble();
-        return bufParamVector;
+    public static double readDouble(String description, Scanner consoleInput) {
+        System.out.print(description);
+        return consoleInput.nextDouble();
     }
 
     private static double f(double x) {
         return tan(2 * x) - 3;
     }
 
-    private static void printTable(double[] parameterVector) {
+    private static void printTable(double a, double b, double h) {
         System.out.println("|x         |f(x)      |");
-        for (double x = parameterVector[0]; x <= parameterVector[1]; x += parameterVector[2]) {
+        for (double x = a; x <= b; x += h) {
             System.out.printf("|%10.2f|%10.2f|\n", x, f(x));
         }
     }
